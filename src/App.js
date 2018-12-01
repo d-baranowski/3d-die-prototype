853bd8b9984343f6e63afaa1e2c60829b66f33fd
Daniel Baranowski <daniel.baranowski@infinityworks.com>
2018-12-01 03:00:40 +0000

    Delete unused files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 import SceneComponent from "./SceneComponent";
-import D20 from "./dice/D20";
 
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.d20 = new D20();
-
-        this.d20.position.x -= 5;
-        this.d20.position.z -= 15;
     }
 
     render() {
         return (
             <div className="App">
                 <SceneComponent
-                    objects={[this.d20]}
                 />
             </div>
         );
